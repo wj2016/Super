455e857505c2cf344c3e2ccf4cf25ea1ac2cc1b8
Jia Wang <wj2016@gmail.com>
2015-08-06 19:18:33 +0200

    [C++] Xcode project

---
+++ LearningCpp/LearningCpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LearningCpp
+//
+//  Created by Jia Wang on 06/08/15.
+//  Copyright (c) 2015 Jia Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
