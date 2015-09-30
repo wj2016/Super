ca1525647fda75e0ba930728a853c5ac26745181
Jia Wang <wj2016@gmail.com>
2015-10-01 01:18:39 +0200

    objc

---
+++ ObjcRuntime/simple1.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char *argv[])
+{
+    NSLog(@"Hello, world");
+    return 0;
+}
---
+++ ObjcRuntime/simple2.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char *argv[])
+{
+    NSLog(@"Hello, %@", [NSString stringWithFormat:@"world"]);
+    return 0;
+}
